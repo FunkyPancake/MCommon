b3dee0a6a580281c011c4627b9265c68bf502749
FunkyPancake <matdeb01685@gmail.com>
2023-06-25 00:24:20 +0200

    add new operators to version

--- CommonTypes/Version.cs
+++ CommonTypes/Version.cs
@@ -1,25 +1,59 @@
 namespace CommonTypes;
 
 public readonly struct Version {
-    private readonly uint _minor;
-    private readonly uint _patch;
-
-    public uint Major { get; }
+    private uint Major { get; }
+    private uint Minor { get; }
+    private uint Patch { get; }
 
     public override string ToString() {
-        return $"{Major}.{_minor}.{_patch}";
+        return $"{Major}.{Minor}.{Patch}";
     }
 
     public Version(uint major, uint minor, uint patch) {
         Major = major;
-        _minor = minor;
-        _patch = patch;
+        Minor = minor;
+        Patch = patch;
     }
 
     public Version(string str) {
         var split = str.Split('.');
         Major = uint.Parse(split[0]);
-        _minor = uint.Parse(split[1]);
-        _patch = uint.Parse(split[2]);
+        Minor = uint.Parse(split[1]);
+        Patch = uint.Parse(split[2]);
+    }
+
+    public static bool operator >(Version a, Version b) {
+        if (a.Major > b.Major)
+            return true;
+        if (a.Major == b.Major) {
+            if (a.Minor > b.Minor)
+                return true;
+            if (a.Minor == b.Minor) {
+                return a.Patch > b.Patch;
+            }
+        }
+
+        return false;
+    }
+
+    public static bool operator <(Version a, Version b) {
+        if (a.Major < b.Major)
+            return true;
+        if (a.Major == b.Major) {
+            if (a.Minor < b.Minor)
+                return true;
+            if (a.Minor == b.Minor) {
+                return a.Patch < b.Patch;
+            }
+        }
+
+        return false;
+    }
+    public static bool operator ==(Version a, Version b) {
+        return a.Major == b.Major && a.Minor == b.Minor && a.Patch == b.Patch;
+    }
+
+    public static bool operator !=(Version a, Version b) {
+        return a.Major != b.Major || a.Minor != b.Minor || a.Patch != b.Patch;
     }
 }
